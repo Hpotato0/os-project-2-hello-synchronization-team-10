39d756bc030213076186933f178fe8f6dea7b497
rurugy15 <rurugy15@gmail.com>
2023-04-14 20:26:33 +0900

    global variables added

--- kernel/rotation_lock.c
+++ kernel/rotation_lock.c
@@ -1,15 +1,19 @@
 #include <linux/syscalls.h>
 #include <linux/spinlock.h>
 #include <linux/list.h>
+#include <linux/semaphore.h>
+#define ROTAION_LOCK_READ 100
+#define ROTATION_LOCK_WRITE 101
 // global variable
 
+int orientation;
 rwlock_t orientation_lock;
 
 struct thread_list {
     int type;
     int low;
     int high;
-    int start;
+    sem_t start;
     long id;
     struct list_head *next;
     struct list_head *prev;
@@ -21,7 +25,11 @@ long set_orientation (int degree){
 }
 
 long rotation_lock(int low, int high, int type){
+    int local_orienation;
+    static long id = 0;
 
+    //(1) struct dynamic alloc and init
+    struct thread_list *new_thread
 }
 
 long rotation_unlock(long id){
